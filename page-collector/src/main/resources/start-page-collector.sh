9236e586f2f20f95fdb2d1d0e5c1ca2b27ffedc4
Amin Borjian <borjianamin98@outlook.com>
2019-08-18 18:25:13 +0430

    Fix jmx port in shells

--- page-collector/src/main/resources/start-page-collector.sh
+++ page-collector/src/main/resources/start-page-collector.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 export BASEDIR=$(dirname "$0")
 export BASEDIR=$BASEDIR/..
-export JAVA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9070 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=localhost"
 export APP_NAME="page-collector-1.0.jar"
+export JMX_PORT="9071"
+export PROMETHEUS_PORT="9107"
+export JAVA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=localhost"
+
 cd $BASEDIR
 
 jar uf lib/$APP_NAME -C conf/ .
 mkdir -p logs
 cd logs
 
-java $JAVA_OPTS -javaagent:../lib/jmx_prometheus.jar=9101:../conf/jmx-promethues.yaml -jar ../lib/$APP_NAME
+java $JAVA_OPTS -javaagent:../lib/jmx_prometheus.jar=$PROMETHEUS_PORT:../conf/jmx-promethues.yaml -jar ../lib/$APP_NAME
