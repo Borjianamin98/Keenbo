63dfe8eafc77ae62fb6daa3d8eb1a28a1b17b0af
Amin Borjian <borjianamin98@outlook.com>
2019-08-21 12:03:58 +0430

    Fix thread monitoring

--- crawler/src/main/java/in/nimbo/monitoring/ThreadsMonitor.java
+++ crawler/src/main/java/in/nimbo/monitoring/ThreadsMonitor.java
@@ -6,12 +6,14 @@ import com.codahale.metrics.SharedMetricRegistries;
 
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadsMonitor implements Runnable {
     private ThreadGroup threadGroup;
-    private int waitingThreads = 0;
-    private int activeThreads = 0;
-    private int totalThreads = 0;
+    private AtomicInteger waitingThreads = new AtomicInteger(0);
+    private AtomicInteger terminatedThreads = new AtomicInteger(0);
+    private AtomicInteger activeThreads = new AtomicInteger(0);
+    private AtomicInteger totalThreads = new AtomicInteger(0);
 
     public ThreadsMonitor(ThreadGroup threadGroup) {
         this.threadGroup = threadGroup;
@@ -20,54 +22,55 @@ public class ThreadsMonitor implements Runnable {
                 new CachedGauge<Integer>(3, TimeUnit.SECONDS) {
                     @Override
                     protected Integer loadValue() {
-                        return getWaitingThreads();
+                        return waitingThreads.get();
                     }
                 });
-        metricRegistry.register(MetricRegistry.name(ThreadsMonitor.class, "runningThreadsGauge"),
+        metricRegistry.register(MetricRegistry.name(ThreadsMonitor.class, "activeThreadsGauge"),
                 new CachedGauge<Integer>(3, TimeUnit.SECONDS) {
                     @Override
                     protected Integer loadValue() {
-                        return getActiveThreads();
+                        return activeThreads.get();
+                    }
+                });
+        metricRegistry.register(MetricRegistry.name(ThreadsMonitor.class, "terminatedThreadsGauge"),
+                new CachedGauge<Integer>(3, TimeUnit.SECONDS) {
+                    @Override
+                    protected Integer loadValue() {
+                        return totalThreads.get();
                     }
                 });
         metricRegistry.register(MetricRegistry.name(ThreadsMonitor.class, "totalThreadsGauge"),
                 new CachedGauge<Integer>(3, TimeUnit.SECONDS) {
                     @Override
                     protected Integer loadValue() {
-                        return getTotalThreads();
+                        return totalThreads.get();
                     }
                 });
     }
 
-    private Integer getTotalThreads() {
-        return totalThreads;
-    }
-
-    private int getWaitingThreads() {
-        return waitingThreads;
-    }
-
-    private int getActiveThreads() {
-        return activeThreads;
-    }
-
     @Override
     public void run() {
-        activeThreads = 0;
-        waitingThreads = 0;
-        totalThreads = 0;
+        int newActiveThreads = 0;
+        int newWaitingThreads = 0;
+        int newTerminatedThreads = 0;
+        int newTotalThreads = 0;
         Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
         for (Thread thread : threadSet) {
             if (thread.getThreadGroup().equals(threadGroup) && thread.getName().equals(threadGroup.getName())) {
-                totalThreads++;
+                newTotalThreads++;
                 Thread.State state = thread.getState();
                 if (state.equals(Thread.State.RUNNABLE)) {
-                    activeThreads++;
-                }
-                else if (state.equals(Thread.State.BLOCKED) || state.equals(Thread.State.TIMED_WAITING) || state.equals(Thread.State.WAITING)){
-                    waitingThreads++;
+                    newActiveThreads++;
+                } else if (state.equals(Thread.State.BLOCKED) || state.equals(Thread.State.TIMED_WAITING) || state.equals(Thread.State.WAITING)) {
+                    newWaitingThreads++;
+                } else if (state.equals(Thread.State.TERMINATED)) {
+                    newTerminatedThreads++;
                 }
             }
         }
+        activeThreads.set(newActiveThreads);
+        waitingThreads.set(newWaitingThreads);
+        terminatedThreads.set(newTerminatedThreads);
+        totalThreads.set(newTotalThreads);
     }
 }
